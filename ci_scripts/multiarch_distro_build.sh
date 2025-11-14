162ea6aa82f7bd11fc293b3639cf8d3be4329e7e
José Mendes <mendesfilho@protonmail.com>
2025-11-14 12:22:44 +0000

    Realsense ros wrapper

--- ci_scripts/multiarch_distro_build.sh
+++ ci_scripts/multiarch_distro_build.sh
@@ -2,7 +2,7 @@
 
 # Skip building for jazzy on jetpack-6.2, invalid configuration
 if [[ $ROS_DISTRO == "jazzy" && $ARCH == "jetpack-6.2" ]]; then
-  exit 0
+    exit 0
 fi
 
 # Make sure you have a builder that supports multi‑arch
@@ -10,25 +10,25 @@ docker buildx create --name multiarch --use
 docker buildx inspect --bootstrap    # Installs QEMU and other helpers
 
 if [[ $ROS_DISTRO == "humble" ]]; then
-  BASE_IMAGE="ubuntu:22.04"
+    BASE_IMAGE="ubuntu:22.04"
 elif [[ $ROS_DISTRO == "jazzy" ]]; then
-  BASE_IMAGE="ubuntu:24.04"
+    BASE_IMAGE="ubuntu:24.04"
 else
-  echo -e "Unsupported ROS_DISTRO: $ROS_DISTRO! Quitting..."
-  exit 1
+    echo -e "Unsupported ROS_DISTRO: $ROS_DISTRO! Quitting..."
+    exit 1
 fi
 
 # Set the platform according to the architecture
 if [[ $ARCH == "aarch64" ]]; then
-  PLATFORM="linux/arm64/v8"
-  BASE_IMAGE="arm64v8/$BASE_IMAGE"
+    PLATFORM="linux/arm64/v8"
+    BASE_IMAGE="arm64v8/$BASE_IMAGE"
 elif [[ $ARCH == "x86_64" ]]; then
-  PLATFORM="linux/amd64"
+    PLATFORM="linux/amd64"
 elif [[ $ARCH == "jetpack-6.2" ]]; then
-  PLATFORM="linux/arm64/v8"
-  BASE_IMAGE="nvcr.io/nvidia/l4t-jetpack:r36.4.0"
+    PLATFORM="linux/arm64/v8"
+    BASE_IMAGE="nvcr.io/nvidia/l4t-jetpack:r36.4.0"
 else
-  PLATFORM=$ARCH
+    PLATFORM=$ARCH
 fi
 
 # Sanitize ARCH before using it as part of the tag name – keep only letters, digits, dashes and periods,
@@ -36,13 +36,15 @@ fi
 ARCH=${ARCH//[^a-zA-Z0-9.-]/-}
 
 if [[ $CI_COMMIT_REF_NAME == $CI_DEFAULT_BRANCH ]]; then
-  if [[ $ARCH == "x86-64" ]]; then
-    IMAGE_NAME="$CI_REGISTRY_IMAGE:${ROS_DISTRO}"
-  else
-    IMAGE_NAME="$CI_REGISTRY_IMAGE:${ROS_DISTRO}-${ARCH}"
-  fi
+    IMAGE_BASE_NAME=$CI_REGISTRY_IMAGE
 else
-  IMAGE_NAME="$CI_REGISTRY_IMAGE/$CI_COMMIT_REF_SLUG:${ROS_DISTRO}-${ARCH}"
+    IMAGE_BASE_NAME="${CI_REGISTRY_IMAGE}/${CI_COMMIT_REF_SLUG}"
+fi
+
+if [[ $ARCH == "x86-64" ]]; then
+    IMAGE_NAME="${IMAGE_BASE_NAME}:${ROS_DISTRO}"
+else
+    IMAGE_NAME="${IMAGE_BASE_NAME}:${ROS_DISTRO}-${ARCH}"
 fi
 
 # Login to the registry
@@ -50,9 +52,9 @@ echo "$REGISTRY_PWD" | docker login -u $REGISTRY_USER --password-stdin $CI_REGIS
 
 # Build and push the image
 docker buildx build \
-  --platform $PLATFORM \
-  --build-arg ROS_DISTRO=$ROS_DISTRO \
-  --build-arg BASE_IMAGE=$BASE_IMAGE \
-  -t $IMAGE_NAME \
-  -t ${CI_REGISTRY_IMAGE}:${CI_JOB_ID} \
-  --push .
+    --platform $PLATFORM \
+    --build-arg ROS_DISTRO=$ROS_DISTRO \
+    --build-arg BASE_IMAGE=$BASE_IMAGE \
+    -t $IMAGE_NAME \
+    -t ${CI_REGISTRY_IMAGE}:${CI_JOB_ID} \
+    --push .
